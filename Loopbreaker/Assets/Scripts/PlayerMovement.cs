2b541f24d7524627f6b90c2d8813cb59680df0ce
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-04-21 12:53:22 +0200

    Reapply "Merge branch 'main' of github.com:KeeganVForsman/Loopbreaker"
    This reverts commit fd432fe86711091f618aa2e82f037acc84e3fb3e.

--- Loopbreaker/Assets/Scripts/PlayerMovement.cs
+++ Loopbreaker/Assets/Scripts/PlayerMovement.cs
@@ -21,10 +21,12 @@ public class PlayerMovement : MonoBehaviour
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
         // Rotate to face movement direction
-        if (moveDirection != Vector3.zero)
+        /*if (moveDirection != Vector3.zero)
         {
-            Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 0.15f);
-        }
+            //Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, moveDirection);
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f,0f, angle-90f);
+            transform.rotation = targetRotation;
+        }*/
     }
 }
