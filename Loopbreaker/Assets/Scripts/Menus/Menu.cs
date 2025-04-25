04e720f389e7d3662daa4d6b2e082e723bd8c1a2
KeeganVForsman <keeganforsman@gmail.com>
2025-04-25 12:33:26 +0200

    menus and input

---
+++ Loopbreaker/Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OrnBoss()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PokBoss()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
